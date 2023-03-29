3c33a287722833496996056f389fe5c1cf5676e3
maks-pavlenkov <coolpavlenkov@mail.ru>
2023-03-29 22:51:44 +0300

    added new models to admin

--- backend/recipies/admin.py
+++ backend/recipies/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 
-from .models import IngredientRecipe, Ingredient, Recipe, Tag, TagRecipe
+from .models import IngredientRecipe, Ingredient, Recipe, Tag, TagRecipe, ShoppingCart, FavoriteRecipes, Following
 
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
+
+class FollowingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    search_fields = ('user', 'following')
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'author')
+    search_fields = ('author',)
+
+
+class FavoriteRecipesAdmin(admin.ModelAdmin):
+    list_display = ('author', 'recipe')
+    search_fields = ('author',)
 
 
 class IngredientAdmin(admin.ModelAdmin):
@@ -34,3 +49,6 @@ class RecipeAdmin(admin.ModelAdmin):
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(FavoriteRecipes, FavoriteRecipesAdmin)
+admin.site.register(Following, FollowingAdmin)
