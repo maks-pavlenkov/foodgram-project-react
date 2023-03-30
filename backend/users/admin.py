791d8f43077e9530bbc0ed46e050e2506c0124b1
maks-pavlenkov <coolpavlenkov@mail.ru>
2023-03-30 21:04:18 +0300

    added tabular in user model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import User
+from recipies.models import ShoppingCart
+
+
+class ShoppingCartInline(admin.TabularInline):
+    model = ShoppingCart
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username',)
     search_fields = ('username', 'email')
     list_filter = ('username', 'email')
+    inlines = (ShoppingCartInline,)
 
 
 admin.site.register(User, UserAdmin)
